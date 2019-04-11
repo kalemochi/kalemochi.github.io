feeaef8603d0363d62c7c616a8daeab8a5a1edcd
Faith Mayan <Faithmayan@gmail.com>
2019-04-11 12:18:46 -0400

    New file

--- Web Design 2/JSON-hw/main.js
+++ Web Design 2/JSON-hw/main.js
@@ -1,7 +1,8 @@
 var ourRequest = = new XMLHttpRequest();
 ourRequest.open('GET','https://data.ny.gov/api/views/6xda-q7ev/rows.json?accessType=DOWNLOAD')
 ourRequest.onload = function() {
-    console.log(ourRequest.responseText);
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0]);
     
 };
 
